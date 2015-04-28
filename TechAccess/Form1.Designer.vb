d4560b4c59eeb29714f892adb9625986d6a1447d
freemasen <r.f.masen@gmail.com>
2015-04-28 17:49:49 -0500

    started on techaccess, added references to other project, very early for this projcet.

--- TechAccess/Form1.Designer.vb
+++ TechAccess/Form1.Designer.vb
@@ -22,9 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstTickets = New System.Windows.Forms.ListBox()
+        Me.btnResolveTicket = New System.Windows.Forms.Button()
+        Me.btnEmail = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(234, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Tickets you have been assigned"
+        '
+        'lstTickets
+        '
+        Me.lstTickets.FormattingEnabled = True
+        Me.lstTickets.ItemHeight = 20
+        Me.lstTickets.Location = New System.Drawing.Point(12, 42)
+        Me.lstTickets.Name = "lstTickets"
+        Me.lstTickets.Size = New System.Drawing.Size(212, 244)
+        Me.lstTickets.TabIndex = 1
+        '
+        'btnResolveTicket
+        '
+        Me.btnResolveTicket.Location = New System.Drawing.Point(599, 672)
+        Me.btnResolveTicket.Name = "btnResolveTicket"
+        Me.btnResolveTicket.Size = New System.Drawing.Size(167, 63)
+        Me.btnResolveTicket.TabIndex = 2
+        Me.btnResolveTicket.Text = "Resolve Ticket"
+        Me.btnResolveTicket.UseVisualStyleBackColor = True
+        '
+        'btnEmail
+        '
+        Me.btnEmail.Location = New System.Drawing.Point(12, 672)
+        Me.btnEmail.Name = "btnEmail"
+        Me.btnEmail.Size = New System.Drawing.Size(167, 63)
+        Me.btnEmail.TabIndex = 3
+        Me.btnEmail.Text = "Email Reporter"
+        Me.btnEmail.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(16, 310)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 747)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnEmail)
+        Me.Controls.Add(Me.btnResolveTicket)
+        Me.Controls.Add(Me.lstTickets)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lstTickets As System.Windows.Forms.ListBox
+    Friend WithEvents btnResolveTicket As System.Windows.Forms.Button
+    Friend WithEvents btnEmail As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
--- TechAccess/Form1.vb
+++ TechAccess/Form1.vb
@@ -1,3 +1,51 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
 
+Public Class Form1
+    'defines to source for all of the tables we will be referencing
+    Dim Connection As New SqlConnection("server=HERMAN-W8\sqlexpress;database=support;Trusted_Connection=Yes")
+
+    'this block also sets up a data adapter and data table to be used
+    'by other parts of the program that need to reference/modify the tickets table
+    Dim dataAdapter As New SqlDataAdapter
+    Dim dataTable As New DataTable
+    Dim techtalbe As New DataTable
+
+    Dim techadapter As New SqlDataAdapter
+
+    Dim currentTech As Tech
+    Dim tickets As New ArrayList
+
+
+    Private Sub btnResolveTicket_Click(sender As Object, e As EventArgs) Handles btnResolveTicket.Click
+        If lstTickets.SelectedIndex <> -1 Then
+
+        End If
+    End Sub
+
+    Private Sub updateLists()
+        If tickets.Count > 0 Then
+            For Each newTicket In tickets
+                lstTickets.Items.Add(newTicket.ToString)
+            Next
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        techadapter = New SqlDataAdapter(String.Format("SELECT * FROM techs WHERE TechID = {0}", 1), Connection)
+        techadapter.Fill(techtalbe)
+        If techtalbe.Rows.Count > 0 Then
+            currentTech = New Tech(CStr(techtalbe.Rows(0).Item(1)), CStr(techtalbe.Rows(0).Item(2)), CStr(techtalbe.Rows(0).Item(3)), techtalbe.Rows(0).Item(4))
+            currentTech.TechID = CInt((techtalbe.Rows(0).Item(0)))
+        End If
+        If IsNothing(currentTech) = False Then
+            dataAdapter = New SqlDataAdapter(String.Format("SELECT * FROM Tickets WHERE assignment = {0}", CInt(currentTech.TechID)), Connection)
+            dataAdapter.Fill(dataTable)
+        End If
+        For Each row As DataRow In dataTable.Rows
+            Dim newTicket As Ticket
+            newTicket = New Ticket(CStr(row.Item(1)), CStr(row.Item(2)), CStr(row.Item(3)), CDate(row.Item(4)), CInt(row.Item(5)), CBool(row.Item(6)))
+            tickets.Add(newTicket)
+        Next
+        updateLists()
+    End Sub
 End Class
